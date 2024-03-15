501726cd2e1bdd8d05d701a54e440877224f872f
dvlpr2003 <gayathrigaya698@gmail.com>
2024-03-15 22:51:42 +0530

    password

--- signup/models.py
+++ signup/models.py
@@ -9,6 +9,7 @@ class Name(models.Model):
 class Signup(models.Model):
     name = models.OneToOneField(Name,on_delete = models.CASCADE,null = False)
     mail = models.EmailField(max_length = 100,null = False,unique = True)
+    password = models.CharField(max_length = 100,null = True)
     phone = models.IntegerField(unique = True, null = False)
     address = models.CharField(max_length = 100,null = False)
     district = models.CharField(max_length = 100,null = False)
