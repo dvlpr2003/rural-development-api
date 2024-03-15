b45589a5425d7337902def4171503b51d0ba776a
dvlpr2003 <gayathrigaya698@gmail.com>
2024-03-15 21:21:29 +0530

    email-login

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from.views import *
+from django.urls import *
 
 route = DefaultRouter()
 route.register("login",LoginSet)
 route.register("signup",SignupSet)
-urlpatterns = route.urls
+urlpatterns = [
+    path("",include(route.urls)),
+    path("email/<str:mail>/",Get_Data_via_Email.as_view())
+]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,9 @@ from .serializers import *
 from login.models import *
 from signup.models import *
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import Http404
 
 
 class SignupSet(ModelViewSet):
@@ -12,3 +15,15 @@ class SignupSet(ModelViewSet):
 class LoginSet(ModelViewSet):
     queryset = Name.objects.all()
     serializer_class = NameSerializers
+
+class Get_Data_via_Email(APIView):
+    def get_object(self, mail):
+        try:
+            return Signup.objects.get(mail=mail)
+        except Signup.DoesNotExist:
+            raise Http404
+    def get(self,request,mail):
+        snippet = self.get_object(mail = mail)
+        serializer = SignupSerializers(snippet)
+        return Response(serializer.data)
+    
