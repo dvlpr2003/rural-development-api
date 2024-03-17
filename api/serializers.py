72b8f1cc7e1a9ef3f47885b5a3db98450bb9675d
dvlpr2003 <gayathrigaya698@gmail.com>
2024-03-17 20:02:05 +0530

    test

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
 from rest_framework import serializers
 from login.models import LoginInfo
 from signup.models import *
+from django.conf import settings
 
 
 class NameSerializers(serializers.ModelSerializer):
@@ -15,6 +17,19 @@ class SignupSerializers(serializers.ModelSerializer):
         fields = "__all__"
     def create(self, validated_data):
         name_data = validated_data.pop('name')
+        mail = validated_data.pop("mail")
         name_instance = Name.objects.create(**name_data)
         signup_instance = Signup.objects.create(name=name_instance, **validated_data)
+        # Send confirmation email
+        subject = "comming from django server"
+        message = "hi da bot uh"
+        from_email = settings.EMAIL_HOST_USER
+        send_mail(
+            subject,
+            message,
+            from_email,
+            [mail],
+            fail_silently=False,
+
+        )
         return signup_instance
--- rural_development_api/settings.py
+++ rural_development_api/settings.py
@@ -122,6 +122,14 @@ USE_I18N = True
 USE_TZ = True
 CORS_ALLOW_ALL_ORIGINS = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = False
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'gayathrigaya698@gmail.com'
+EMAIL_HOST_PASSWORD = 'cqmnqrefilxbvdrk'
+
 
 
 # Static files (CSS, JavaScript, Images)
