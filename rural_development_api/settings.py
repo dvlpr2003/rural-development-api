6cada37a47953c0d8482b4bdbe5aee035b73b3ed
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-07 17:24:38 +0530

    aws integration

--- rural_development_api/settings.py
+++ rural_development_api/settings.py
@@ -142,10 +142,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-AWS_ACCESS_KEY_ID = 'AKIA5FTZAA74ANS5CIPK'
-AWS_SECRET_ACCESS_KEY = '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'
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'uniquebee203'
 AWS_S3_REGION_NAME = 'eu-north-1'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/' 
