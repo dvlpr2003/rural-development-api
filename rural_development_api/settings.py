b5850bbf3236fbbf9d83ab203cc97ac0abd3b944
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-07 17:50:08 +0530

    aws

--- rural_development_api/settings.py
+++ rural_development_api/settings.py
@@ -143,8 +143,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+import boto3
+
+s3_client = boto3.client('s3')
+
+
 AWS_STORAGE_BUCKET_NAME = 'uniquebee203'
 AWS_S3_REGION_NAME = 'eu-north-1'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/' 
